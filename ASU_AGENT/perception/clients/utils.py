4570a160592d93d7a8c8030cae2e2b824aa2baf8
Loay <hythamconn1@gmail.com>
2021-03-09 00:55:12 +0200

    checkpoint after successfull yolo-detection

--- ASU_AGENT/perception/clients/utils.py
+++ ASU_AGENT/perception/clients/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import glob
 import cv2
 
 
@@ -31,8 +32,6 @@ def clean_world(actors_list):
     print("-"*11, "Cleaned All!", "-"*11)
    
 
-
-
 def preprocess_frame(frame, shape):
     frame = np.array(frame.raw_data)
     frame = frame.reshape(shape)
@@ -58,3 +57,57 @@ def save_offline_video(offline_frames, fps=20.0):
         video_writer.write(frame)
     video_writer.release()
     print("-"*11, "saved offline video successfully!", "-"*11)
+
+
+def unpack_video_frames(video_path, output_path):
+    cap = cv2.VideoCapture(video_path)
+    i = 0
+    print("-"*11, "Unpacking frames...!", "-"*11)
+    while True:
+        ret, frame = cap.read()
+        print(ret)
+        if not ret:
+            break
+        else:
+            # cv2.imwrite(output_path+"%06d.png"%i, frame)
+            print("-"*11, r"frame %06d"%i, "-"*11, end="")
+            i += 1
+    cap.release()
+
+
+# src_pth = "data/mono_camera/rgb_video/output2.mp4"
+# dst_pth = "data/mono_camera/rgb_frames/"
+# cap = cv2.VideoCapture(src_pth)
+# i = 0
+# print("-"*11, "Unpacking frames", "-"*11)
+# while True:
+#     ret, frame = cap.read()
+#     # print(ret)
+#     if not ret:
+#         print("\n", "-"*11, "Unpacking frames completed", "-"*11)
+#         break
+
+#     else:
+#         cv2.imwrite(dst_pth+"%06d.png"%i, frame)
+#         print("\r\t\tframe %06d"%i, end="")
+#         i += 1
+# print("\n")
+# cap.release()
+
+import json
+import os
+
+def pack_yolo_dataset(imgs_path):
+    labels = {}
+    with open("img_lbl.json", "w") as all_labels:
+        for img_label_path in glob.glob(imgs_path+"*.txt"):
+            if not img_label_path.endswith("classes.txt"):
+                with open(img_label_path, "r") as img_label:
+                    img_objects = img_label.readlines()
+                    img_name = os.path.basename(img_label_path).split(".")[0]
+                    labels[img_name] = img_objects
+    print(labels)
+
+
+pth = "../../../ASU_AGENT/perception/detectors/ObjectDetection/yolov3/data/carla_data/rgb_frames/"
+pack_yolo_dataset(pth)
