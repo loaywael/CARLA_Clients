afad9cef37d0ee1abffb3d1aaf1d0e008aee7bc2
Loay <hythamconn1@gmail.com>
2021-03-09 20:59:08 +0200

    completed the depth estimator

--- ASU_AGENT/perception/clients/services/depth_estimation.py
+++ ASU_AGENT/perception/clients/services/depth_estimation.py
@@ -9,8 +9,8 @@ class DepthEstimator:
         self.__rMatrix = right_matrix
         self.__lK, self.__lR, self.__lT = self.decompose_projection(self.__lMatrix)
         self.__rK, self.__rR, self.__rT = self.decompose_projection(self.__rMatrix)
-        self.__f = self.lK[0, 0]
-        self.__b = abs(self.rT[1] - self.lT[1])
+        self.__f = self.__lK[0, 0]
+        self.__b = abs(self.__rT[1] - self.__lT[1])
         self.__stereo_matcher = cv2.StereoSGBM_create(
             minDisparity=0,
             numDisparities=6*16,
@@ -22,16 +22,16 @@ class DepthEstimator:
 
     @staticmethod
     def decompose_projection(Matrix):
-        K, R, t, _, _ = cv2.cv2.decomposeProjectionMatrix(Matrix)
+        K, R, t, _, _, _, _ = cv2.decomposeProjectionMatrix(Matrix)
         return K, R, t/t[3]
 
-    @staticmethod
-    def comp_disparity(left, right):
+    def comp_disparity(self, left, right):
         desparity_map = self.__stereo_matcher.compute(left, right)
         return desparity_map.astype("float32") / 16
 
     def __call__(self, left, right):
-        disparity_map = comp_disparity(left, right)
-        disparity_map[disparity_map == 0 or disparity_map ==-1] = 0.01
-        depthmap = f * b / disparity_map
+        disparity_map = self.comp_disparity(left, right)
+        disparity_map[disparity_map == 0] = 0.01
+        disparity_map[disparity_map ==-1] = 0.01
+        depthmap = self.__f * self.__b / disparity_map
         return depthmap
